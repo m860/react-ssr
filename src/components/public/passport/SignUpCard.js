feda7aaffa50d237f9c6d4d7586b2b534f2c1eb8
jean <jean.ma.1986@gmail.com>
2017-10-30 09:38:02 +0800

    add Passport

---
+++ src/components/public/passport/SignUpCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Base from '../../Base'
+import PropTypes from 'prop-types'
+
+export default class SignUpCard extends Base {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
