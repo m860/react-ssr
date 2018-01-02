7890253f912cba49f080597d5af67f4d22700bfe
jean <jean.ma.1986@gmail.com>
2018-01-02 14:31:24 +0800

    add Toast test

--- test/Toast.test.js
+++ test/Toast.test.js
@@ -2,14 +2,24 @@ import raf from '../__mocks__/tempPolyfill'
 import React from "react";
 import Enzyme, {mount} from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import App from '../src/components/App'
+import App, {store} from '../src/components/App'
+import {showToast} from '../src/ar/toast.ar'
 
 Enzyme.configure({adapter: new Adapter()});
 
 describe(`test Toast`, () => {
 	const app = mount(<App/>);
-	test('store is ready', () => {
-		expect(app.state.storeIsReady);
+	test('show toast & hide toast after 1 second', async () => {
+		store.dispatch(showToast({
+			message: 'abc',
+			duration: 1000
+		}));
+		const items = app.find('toast-item');
+		expect(items.length === 1);
+		setTimeout(() => {
+			expect(app.find('toast-item').length === 0);
+			return Promise.resolve();
+		}, 1000);
 	})
 })
 
